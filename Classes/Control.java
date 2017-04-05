7375c04054bbde1a42a2537e1ce96ece26a6d337
Luzenildo Junior <luzenildojunior@lavid.ufpb.br>
2017-04-05 09:35:49 -0300

    Plain Controll class implemented

---
+++ Classes/Control.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Control{
+
+	ArrayList<Usuario> users = new Arraylist<Usuario>();
+
+	public Control(){
+
+	}
+
+	public void addUser(Usuario user){
+		users.add(user);
+	}
+
+	public void removeUser(){
+
+	}
+
+	public void searchUser(){
+
+	}
+}
